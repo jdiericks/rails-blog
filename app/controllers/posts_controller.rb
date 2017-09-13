29c92775fd9007a030ded78c6af23a13de697cec
JonLuke Diericks <jdiericks@sogomarketingagency.com>
2017-09-13 16:25:56 +0000

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,60 @@
+class PostsController < ApplicationController
+    before_action :find_post, only: [:edit, :update, :show, :delete]
+    
+    def index
+        @posts = Post.all
+    end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new
+        if @post.save(post_params)
+            flash[:notice] = "Successfully created the post!"
+            redirect_to post_path(@post)
+        else
+            flash[:alert] = "There was an error creating the post!"
+            render :new
+        end
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        if @post.update_attributes(post_params)
+            flash[:notice] = "Successfully updated the post!"
+            redirec_to post_path(@posts)
+        else
+            flash[:alert] = "There was an error in updating the post!"
+            render :edit
+        end
+    end
+    
+    
+    def show
+        
+    end
+    
+    def destroy
+        if @post.destroy
+            flash[:notice] = "Successfully deleted the post!"
+            redirect_to posts_path
+        else
+            flash[:alert] = "Error in deleting the post!"
+        end
+    end
+    
+    private
+    
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
+    
+    def find_post
+        @post = Post.find(params[:id])
+    end
+end
